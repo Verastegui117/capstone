85423e6e8e1e0f20d785a108567b1886d76e41d8
Sean <spmccarthy4@gmail.com>
2023-09-26 19:28:35 -0700

    bare bones

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
+
+function Navbar() {
+    const navRef = useRef();
+    const showNavBar = () => {
+      navRef.current.classList.toggle("responsive_nav");
+    }
+    return (
+          <header>
+            <h3>Logo</h3>
+            <nav ref={navRef}>
+              <a href='/'>Home</a>
+              <a href='/Login'>Login/Sign Up</a>
+              <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+                <FaTimes />
+            </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavBar}>
+              <FaBars />
+            </button>
+            <input className='search' placeholder='search'></input>
+
+        </header>
+          
+      );
+    }
+    
+
+export default Navbar;
---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,12 @@
+import './../App.css';
+import Navbar from "./../components/Navbar";
+
+function Home() {
+  return (
+    <div className='body'>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
