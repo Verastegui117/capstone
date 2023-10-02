69d4f653a0b4dfeb10a958d5d2fff88eb7dd6968
Sean <spmccarthy4@gmail.com>
2023-10-02 12:18:30 -0700

    base template

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
-import Navbar from './components/Navbar'; // Import the Navbar component
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Include the Navbar component */}
       <Home />
       {/* Add additional components/routes here */}
     </div>
   );
 }
 
-
 export default App;
+
